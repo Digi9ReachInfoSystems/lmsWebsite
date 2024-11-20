64bcba92e84b03f85deb6029d8d198c003b21db6
jayanthbr29 <jayanthbr@digi9.co.in>
2024-11-20 16:06:16 +0530

    student module

--- lmswebsite/src/module/teacher/pages/BecomeTeacherApplicationForm/BecomeTeacherApplicationForm.jsx
+++ lmswebsite/src/module/teacher/pages/BecomeTeacherApplicationForm/BecomeTeacherApplicationForm.jsx
@@ -7,11 +7,7 @@ import BecomeTeacherLogo from "../../assets/BecomeTeacherLogo.png";
 import { getTeacherApplicationsByUserId, submitTeacherApplication } from "../../../../api/teachersApplicationApi";
 import TeachersSection from "../../components/TeacherSection/TeachersSection";
 import FooterTeacher from "../../components/Footer/FooterTeacher";
-<<<<<<< HEAD
 import { getAllClasses, getClassesByBoardId } from "../../../../api/classApi";
-=======
-import { getAllClasses } from "../../../../api/classApi";
->>>>>>> b6af5d5 (Teacher Module)
 import {
   ApplicationContainer,
   Form,
@@ -27,14 +23,9 @@ import {
   getTeachersBySubjectAndClass,
   getStudentsBySubjectAndClass,
 } from "../../../../services/createBatch";
-<<<<<<< HEAD
 import { updateUserByAuthId } from "../../../../api/userApi"
 import api from "../../../../config/axiosConfig";
 import { getBoards } from "../../../../api/boardApi";
-=======
-import {updateUserByAuthId} from "../../../../api/userApi"
-import api from "../../../../config/axiosConfig";
->>>>>>> b6af5d5 (Teacher Module)
 
 const BecomeTeacherApplicationForm = () => {
   const [formVisibility, setFormVisibility] = useState(true);
@@ -44,11 +35,8 @@ const BecomeTeacherApplicationForm = () => {
   const [subjects, setSubjects] = useState([]);
   const [classes, setClasses] = useState([]);
   const [selectedClass, setSelectedClass] = useState([]);
-<<<<<<< HEAD
   const [boardData, setBoardData] = useState([]);
   const [selectedBoard, setSelectedBoard] = useState("");
-=======
->>>>>>> b6af5d5 (Teacher Module)
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -83,17 +71,9 @@ const BecomeTeacherApplicationForm = () => {
     const session = JSON.parse(localStorage.getItem("sessionData"));
     const apicaller = async () => {
       // const subjectData = await getAllSubjects();
-<<<<<<< HEAD
       const board = await getBoards();
       setBoardData(board);
 
-=======
-      const classData = await getAllClasses();
-      // setClasses(classData);
-      // console.log("kk", subjectData);
-      setClasses(classData);
-      console.log("classes", classData);
->>>>>>> b6af5d5 (Teacher Module)
       // setSubjects(subjectData);
       const userresponse = await getUserByAuthId(session.userId);
       try {
@@ -125,7 +105,6 @@ const BecomeTeacherApplicationForm = () => {
     }
     apicaller();
   }, []);
-<<<<<<< HEAD
 
   useEffect(() => {
     const apicaller = async () => {
@@ -137,8 +116,6 @@ const BecomeTeacherApplicationForm = () => {
     apicaller();
   }, [selectedBoard]);
 
-=======
->>>>>>> b6af5d5 (Teacher Module)
   useEffect(() => {
     if (selectedClass.length > 0) {
       const fetchSubjects = async () => {
@@ -147,17 +124,10 @@ const BecomeTeacherApplicationForm = () => {
           console.log("sss data", data);
           setSubjects(subjects.concat(data));
         })
-<<<<<<< HEAD
         console.log("inside ", subjects);
       };
       fetchSubjects();
     } else {
-=======
- console.log("inside ",subjects);
-      };
-      fetchSubjects();
-    }else{
->>>>>>> b6af5d5 (Teacher Module)
       setSubjects([]);
     }
   }, [selectedClass]);
@@ -170,7 +140,6 @@ const BecomeTeacherApplicationForm = () => {
       setTimeout(() => setSuccess(null), 3000);
       setFormData((prev) => ({ ...prev, subject_id: slectedSubject.map((option) => option.value) }));
       setFormData((prev) => ({ ...prev, class_id: selectedClass.map((option) => option.value) }));
-<<<<<<< HEAD
       setFormData((prev) => ({ ...prev, board_id: selectedBoard }));
       setTimeout(() => setSuccess(null), 3000);
       const authId = JSON.parse(localStorage.getItem("sessionData")).userId;
@@ -188,23 +157,6 @@ const BecomeTeacherApplicationForm = () => {
         resume_link: formData.resume,
         profileImage: formData.profileImage,
         board_id: formData.board_id
-=======
-      setTimeout(() => setSuccess(null), 3000);
-      const authId=JSON.parse(localStorage.getItem("sessionData")).userId;
-      const responseUser = await updateUserByAuthId(authId,{name:formData.name,phone_number:formData.phone_number});
-      const submissionData={
-        phone_number:formData.phone_number,
-        class_id:formData.class_id,
-        subject_id:formData.subject_id,
-        state:formData.state,
-        city:formData.city,
-        pincode:formData.pincode,
-        current_position:formData.current_position,
-        experience:formData.experience,
-        language:formData.language,
-        resume_link:formData.resume,
-        profileImage:formData.profileImage
->>>>>>> b6af5d5 (Teacher Module)
       }
       const response = await submitTeacherApplication(submissionData);
 
@@ -355,7 +307,6 @@ const BecomeTeacherApplicationForm = () => {
               </div>
               <div className="applicationRowThree">
                 <Select
-<<<<<<< HEAD
                   placeholder="Select Board"
                   className="react-select-container"
                   classNamePrefix="react-select"
@@ -370,8 +321,6 @@ const BecomeTeacherApplicationForm = () => {
               <div className="applicationRowThree">
                 {selectedBoard &&
                   <Select
-=======
->>>>>>> b6af5d5 (Teacher Module)
                   isMulti
                   placeholder="Select classes..."
                   className="react-select-container"
@@ -383,17 +332,10 @@ const BecomeTeacherApplicationForm = () => {
                   onChange={(options) => {
                     setSelectedClass(options);
                   }}
-<<<<<<< HEAD
                 />}
               </div>
               <div className="applicationRowThree">
                 {console.log("Subjects:", subjects)}
-=======
-                />
-              </div>
-              <div className="applicationRowThree">
-              {console.log("Subjects:", subjects)}
->>>>>>> b6af5d5 (Teacher Module)
                 {subjects &&
 
                   <Select
