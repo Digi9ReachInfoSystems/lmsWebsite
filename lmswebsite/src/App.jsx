73afc9f30072f140ead42e4ebfb7abc1c11a12a4
Flutterneu <rsmx141@gmail.com>
2024-11-27 18:51:56 +0530

    new updated teacher schedule

--- lmswebsite/src/App.jsx
+++ lmswebsite/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, createRoutesFromElements ,createBrowserRouter} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  createRoutesFromElements,
+  createBrowserRouter,
+} from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./style/theme/theme"; // Import your theme
 import { GlobalStyles } from "./style/GlobalStyles/GlobalStyles"; // Import your global styles
@@ -15,7 +21,7 @@ import TeacherApplicationFormView from "./module/admin/page/ApplicationFormView/
 import TeacherApplicationFormReview from "./module/admin/page/TeachersApplicationFormReview/TeacherApplicationFormReview";
 import Circulars from "./module/admin/page/Circular/Circulars";
 import CreateCircular from "./module/admin/page/CreateCircular/CreateCircular";
-import CustomerQuery from "./module/admin/page/CustomerQuery/CustomerQuery"
+import CustomerQuery from "./module/admin/page/CustomerQuery/CustomerQuery";
 import CustomerQueryFormView from "./module/admin/page/CustomerQueryViewForm/CustomerQueryViewForm";
 import BecomeTeacherApplicationForm from "./module/teacher/pages/BecomeTeacherApplicationForm/BecomeTeacherApplicationForm";
 import LandingPage from "./components/common/LandingPage";
@@ -34,10 +40,9 @@ import SettingsTabs from "./module/teacher/pages/Settings/SettingTapPage/Setting
 import QuizBatches from "./module/teacher/pages/Quizz/QuizzBatches/QuizBatches";
 import QuizList from "./module/teacher/pages/Quizz/QuizList/QuizList";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
-import{ StudentLandingPage} from "./module/student/pages/StudentLandingPage/StudentLandingPage";
+import { StudentLandingPage } from "./module/student/pages/StudentLandingPage/StudentLandingPage";
 import StudentCourseDetailsPage from "./module/student/pages/StudentCourseDetailsPage/StudentCourseDetailsPage";
 import SubscriptionSuccess from "./module/student/pages/SubscriptionSuccess/SubscriptionSuccess";
-
 
 // import PublicRoute from "./module/admin/components/PublicRoute";
 import AssignedBatchStudentsList from "./module/teacher/pages/AssignedBatchStudentsList/AssignedBatchStudentsList";
@@ -54,9 +59,10 @@ import CustomPackage from "./module/admin/page/CustomPackage/CustomPackage";
 import PaymentSuccess from "./module/student/pages/PaymentSuccess/PaymentSuccess";
 import StudentDashboardLayout from "./module/student/pages/StudentDashboardLayout/StudentDashboardLayout";
 import StudentDashboardScreen from "./module/student/pages/StudentDashboard/StudentDashboardScreen";
+import ManageMeeting from "./module/teacher/pages/manageMeetings/manageMeeting";
 function App() {
   const [count, setCount] = useState(0);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <Router>
@@ -80,16 +86,24 @@ function App() {
               </PublicRoute>
             }
           />
-          <Route path="/signup" element={<SignUpPage/>} />
-          <Route path="/student"   element={<StudentLandingPage />} />
-          <Route path="/student/course/details" element={<StudentCourseDetailsPage/>}/>
-          <Route path="/student/package/successPage/" element={<SubscriptionSuccess/>}/>
-          <Route path="/student/package/paymentSucces" element={<PaymentSuccess/>}/>
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/student" element={<StudentLandingPage />} />
+          <Route
+            path="/student/course/details"
+            element={<StudentCourseDetailsPage />}
+          />
+          <Route
+            path="/student/package/successPage/"
+            element={<SubscriptionSuccess />}
+          />
+          <Route
+            path="/student/package/paymentSucces"
+            element={<PaymentSuccess />}
+          />
 
           <Route path="/student/dashboard" element={<StudentDashboardLayout />}>
-          <Route index element={<StudentDashboardScreen/>} />
+            <Route index element={<StudentDashboardScreen />} />
           </Route>
-
 
           <Route path="/teacher" element={<BecomeTeacherApplicationForm />} />
           <Route
@@ -101,12 +115,34 @@ function App() {
             }
           >
             <Route index element={<TeacherDashboardScreen />} />
-            <Route path="/teacher/dashboard/batches" element={<AssignedTeacherBatch />} />
-            <Route path="/teacher/dashboard/assigned-batches/:batchId" element={<AssignedBatchStudentsList />} />
-            <Route path="/teacher/dashboard/circular" element={<TeacherCircular />} />
-            <Route path="/teacher/dashboard/setting" element={<SettingsTabs />} />
-            <Route path="/teacher/dashboard/quizz/assignedBatch" element={<QuizBatches />} />
-            <Route path="/teacher/dashboard/quizz/batches/:batchId" element={<QuizList />} />
+            <Route
+              path="/teacher/dashboard/batches"
+              element={<AssignedTeacherBatch />}
+            />
+            <Route
+              path="/teacher/dashboard/assigned-batches/:batchId"
+              element={<AssignedBatchStudentsList />}
+            />
+            <Route
+              path="/teacher/dashboard/circular"
+              element={<TeacherCircular />}
+            />
+            <Route
+              path="/teacher/dashboard/meetings"
+              element={<ManageMeeting />}
+            />
+            <Route
+              path="/teacher/dashboard/setting"
+              element={<SettingsTabs />}
+            />
+            <Route
+              path="/teacher/dashboard/quizz/assignedBatch"
+              element={<QuizBatches />}
+            />
+            <Route
+              path="/teacher/dashboard/quizz/batches/:batchId"
+              element={<QuizList />}
+            />
           </Route>
           <Route
             path="/admin"
@@ -139,17 +175,34 @@ function App() {
             <Route path="/admin/createcircular" element={<CreateCircular />} />
             <Route path="/admin/customerQueries" element={<CustomerQuery />} />
 
-
             <Route path="/admin/manageContent" element={<ManageContent />}>
-          <Route index element={<ManageContentTable contentType="class" />} />
-          <Route path="subject" element={<ManageContentTable contentType="subject" />} />
-          <Route path="board" element={<ManageContentTable contentType="board" />} />
-          <Route path="package" element={<ManageContentTable contentType="package" />} />
-          <Route path="faq" element={<ManageContentTable contentType="faq" />} />
-          <Route path="banner" element={<ManageContentTable contentType="banner" />} />
-        </Route>
-     
-     <Route path="/admin/customPackage" element={<CustomPackage  />} />
+              <Route
+                index
+                element={<ManageContentTable contentType="class" />}
+              />
+              <Route
+                path="subject"
+                element={<ManageContentTable contentType="subject" />}
+              />
+              <Route
+                path="board"
+                element={<ManageContentTable contentType="board" />}
+              />
+              <Route
+                path="package"
+                element={<ManageContentTable contentType="package" />}
+              />
+              <Route
+                path="faq"
+                element={<ManageContentTable contentType="faq" />}
+              />
+              <Route
+                path="banner"
+                element={<ManageContentTable contentType="banner" />}
+              />
+            </Route>
+
+            <Route path="/admin/customPackage" element={<CustomPackage />} />
             <Route
               path="/admin/customerQueries/:queryId"
               element={<CustomerQueryFormView />}
