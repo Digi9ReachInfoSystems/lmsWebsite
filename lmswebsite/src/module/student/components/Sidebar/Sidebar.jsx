06b7c4dee9b7dc75e9988b73d2510a5cf3c7513c
VasudevChandra082002 <vasudev@digi9.co.in>
2024-12-04 15:09:28 +0530

    Student

--- lmswebsite/src/module/student/components/Sidebar/Sidebar.jsx
+++ lmswebsite/src/module/student/components/Sidebar/Sidebar.jsx
@@ -1,151 +1,210 @@
-import styled from "styled-components";
-
-import { media } from "../../../../style/theme/theme";
-
-export const SideBarwrap = styled.div`
-  background: ${(props) => props.theme.colors.mistyRose};
-  padding: 20px 16px;
-  box-shadow: 0 0.125rem 0.25rem rgba(165, 163, 174, 0.3);
-  width: 260px;
-  display: flex;
-  flex-direction: column;
-  border-top-right-radius: 12px;
-  border-bottom-right-radius: 12px;
-  z-index: 1000;
-  transition: all 300ms ease-in-out;
-  left: 0;
-  top: 0;
-  // position: fixed;
-
-  ${media.xl`
-    transform: translateX(${(props) => (props.isOpen ? "0" : "-100%")});
-  `}
-
-  z-index: 1000 ${media.xxxl`
-    width: 240px;
-  `} ${media.xxl`
-    width: auto;
-    padding: 20px 12px;
-  `} ${media.xl`
-    width: 244px;
-    position: fixed;
-    left:0;
-    top:0;
-    height:100%;
-
-  `} 
-  .sidebar-top {
-    margin-bottom: 32px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
-
-  .sidebar-bottom {
-    flex: 1;
-  }
-
-  .sidebar-brand {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    column-gap: 12px;
-  }
-
-  .brand-logo {
-    background: ${(props) => props.theme.colors.cadet};
-    border-radius: 8px;
-    width: 50px;
-    height: 40px;
-    display: flex;
-    place-content: center;
-    img {
-      width: 44px;
-    }
-  }
-
-  .brand-text {
-    font-size: 24px;
-    font-weight: 600;
-    color: ${(props) => props.theme.colors.cadet};
-
-    ${media.xxl`
-    display:none;`}
-
-    ${media.xl`
-        display:inline;`}
-
-    .sidebar-close-btn {
-      color: ${(props) => props.theme.colors.gray700};
-      display: none;
-      cursor: pointer;
-
-      ${media.xl`
-        display:inline-flex;`}
-    }
-  }
-  .menu-list {
-    display: grid;
-    row-gap: 6px;
-  }
-
-  .menu-item {
-    list-style: none; // Ensures no bullets
-  }
-
-  .menu-link {
-    height: 44px;
-    display: flex;
-    align-items: center;
-    column-gap: 14px;
-    padding: 2px 20px;
-    font-weight: 500;
-    border-radius: 8px;
-    transition: background 0.3s, box-shadow 0.3s;
-
-    ${media.xxl`
-    padding: 2px 10px;
-  `}
-
-    .menu-link-icon {
-      width: 20px;
-      ${media.xxl`
-      width: 24px;
-    `}
-      ${media.xl`
-      width: 20px;
-    `}
-    }
-
-    .menu-link-text {
-      color: ${(props) => props.theme.colors.gray700};
-
-      ${media.xxl`
-      display: none;
-    `}
-      ${media.xl`
-      display: inline;
-    `}
-    }
-
-    &:hover {
-      background: rgba(0, 0, 0, 0.1);
-      border-radius: 8px;
-    }
-
-    &.active {
-      background: ${(props) => props.theme.colors.pink};
-      border-radius: 8px;
-      box-shadow: 0 0.125rem 0.25rem rgba(165, 163, 174, 0.3);
-
-      .menu-link-icon {
-        filter: invert(1) brightness(100);
+import React, { useState, useEffect } from "react";
+import { RxDashboard } from "react-icons/rx";
+import CloseIcon from "@mui/icons-material/Close";
+// import { IconButton } from "@mui/material";
+ 
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  IconButton,
+} from "@mui/material";
+import {
+  Dashboard,
+  AddBox,
+  Assignment,
+  AccessTime,
+  Person,
+  Mail,
+  Description,
+  Settings,
+  Menu as MenuIcon,
+} from "@mui/icons-material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { SideBarwrap } from "./Sidebar.styles";
+import { Link, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FaLayerGroup } from "react-icons/fa6";
+import { MdOutlineAssignment } from "react-icons/md";
+import { VscPreview } from "react-icons/vsc";
+import { RiCustomerServiceLine } from "react-icons/ri";
+import { PiListChecksThin } from "react-icons/pi";
+// import { MdOutlineAssignment } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa6";
+import { AiTwotoneNotification } from "react-icons/ai";
+import { MdVideoCall } from "react-icons/md";
+import { MdOutlineSettings } from "react-icons/md";
+import { FaUsersGear } from "react-icons/fa6";
+import { VscSignOut } from "react-icons/vsc";
+import { MdPayment } from "react-icons/md";
+ 
+const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState("Dashboard");
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(true);
+ 
+ 
+  const handleLogout = () => {
+    localStorage.clear(); // Clear all items in localStorage
+    navigate("/login"); // Redirect to the login page or desired route
+  };
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) {
+        setIsOpen(false);
+      } else {
+        setIsOpen(true);
       }
-
-      .menu-link-text {
-        font-weight: 600;
-        color: ${(props) => props.theme.colors.white};
-      }
-    }
-  }
-`;
+    };
+ 
+    window.addEventListener("resize", handleResize);
+ 
+    // Initial check
+    handleResize();
+ 
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+ 
+  return (
+    <>
+      <div className="sidebar-toggle-btn"></div>
+      <SideBarwrap isOpen>
+        <div className="sidebar-top">
+          <div className="sidebar-brand">
+            <span className="brand-logo">
+              <img
+                src="https://www.logoai.com/oss/icons/2021/10/27/rA73APprj8wskQ0.png"
+                alt="Logo"
+                className="logo"
+              />
+            </span>
+            <div className="sidebar-logo">
+              <IconButton onClick={() => setIsOpen(!isOpen)}>
+                {isOpen ? <CloseIcon /> : <MenuIcon />}
+              </IconButton>
+            </div>
+          </div>
+        </div>
+        <div className="sidebar-body">
+          <div className="sidebar-menu">
+            <ul className="menu-list">
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                    <RxDashboard />
+                  </span>
+                  <span className="menu-link-text">DashBoard</span>
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/assignedBatches"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                    <FaLayerGroup />
+                  </span>
+                  <span className="menu-link-text">Manage Batches</span>
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/taskBoard"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                    <MdOutlineAssignment />
+                  </span>
+                  <span className="menu-link-text">Task Management</span>
+                </NavLink>
+              </li>
+ 
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/circular"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                    <AiTwotoneNotification />
+                  </span>
+                  <span className="menu-link-text">Circulars</span>
+                </NavLink>
+              </li>
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/attendance"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                  <PiListChecksThin />
+                  </span>
+                  <span className="menu-link-text">Attendance</span>
+                </NavLink>
+              </li>
+ 
+             
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/meetings"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                  <MdVideoCall />
+                  </span>
+                  <span className="menu-link-text">Meeting</span>
+                </NavLink>
+              </li>
+             
+             
+              <li className="menu-item">
+                <NavLink
+                  to="/student/dashboard/setting"
+                  activeClassName="active"
+                  className="menu-link"
+                >
+                  <span className="menu-link-icon">
+                    <MdOutlineSettings />
+                  </span>
+                  <span className="menu-link-text">Settings</span>
+                </NavLink>
+              </li>
+             
+             
+           
+           
+           
+ 
+             
+              <li className="menu-item">
+                <NavLink
+                  to="/login" // Optional: Redirect route after logout
+                  className="menu-link"
+                  onClick={handleLogout} // Logout function attached here
+                >
+                  <span className="menu-link-icon">
+                    <VscSignOut />
+                  </span>
+                  <span className="menu-link-text">Sign Out</span>
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </SideBarwrap>
+    </>
+  );
+};
+ 
+export default Sidebar;
+ 
